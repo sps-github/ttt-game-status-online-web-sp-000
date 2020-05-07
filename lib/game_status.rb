791a57d592fd9fdc8801a017f71c0bc696a89a11
Sarah Peters <sarahpieters15@gmail.com>
2020-05-07 12:44:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,7 +51,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if draw?(board) || won?(board) || full?(board) 
+  if draw?(board) || won?(board) || full?(board)
     return true
   else
     return false
