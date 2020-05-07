d71a91778464b6c49b71500f148e0e0ac36f9b75
Sarah Peters <sarahpieters15@gmail.com>
2020-05-07 12:35:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,17 +16,17 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |single_win_combo|
-    win_index_1 = single_win_combo[0]
-    win_index_2 = single_win_combo[1]
-    win_index_3 = single_win_combo[2]
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      return single_win_combo
+      return win_combination
     end
   end
   return false
