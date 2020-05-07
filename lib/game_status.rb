04537a46d756bfee8883adf436a9d0422de370c5
Sarah Peters <sarahpieters15@gmail.com>
2020-05-07 12:32:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,8 +26,8 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    else
-   false
- end
-end
+        return win_combination # return the win_combination indexes that won.
+      else
+        false
+      end
+    end
